a8b2650a6bbe80d469104218df75be1251bc291b
Dong Liang <sayiongod@gmail.com>
2023-05-30 00:16:37 -0400

    finished pt1

--- Projects/Amazon Project/scripts/amazon.js
+++ Projects/Amazon Project/scripts/amazon.js
@@ -77,5 +77,10 @@ document.querySelectorAll('.js-add-to-cart').forEach( (button) => button.addEven
     });
   }
 
-  console.log(cart);
+  let cartQuantity = 0;
+  cart.forEach( (item) => {
+    cartQuantity += item.quantity;
+  });
+
+  document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
 }) );
