2017b2f1705e3bba7dcfb8c1f8cdb7f9a7477028
Dong Liang <sayiongod@gmail.com>
2023-07-18 23:40:29 -0400

    last part

--- Projects/React Learn/src/Results.jsx
+++ Projects/React Learn/src/Results.jsx
@@ -13,7 +13,7 @@ const Results = ({ pets }) => {
             name={pet.name}
             breed={pet.breed}
             images={pet.images}
-            location={`${pet.city},${pet.state}`}
+            location={`${pet.city}, ${pet.state}`}
             key={pet.id}
           />
         ))
